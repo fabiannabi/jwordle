d4f2481a3782cc9760a235b32e4278ede1dafb0f
fabian <fabiannabi@yahoo.com.mx>
2022-02-26 20:08:21 -0600

    Implement simple controlled component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+
+  const [state, setState] = useState({
+    w_1:"", 
+    w_2:""
+  }); 
+
+ function checkWord (event) {
+  event.preventDefault()
+  console.log(state)
+ }
+
+ function handleInputChange(event){
+  event.preventDefault()
+  const value =  event.target.value
+  setState( {...state,[event.target.name]:value  } );
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={checkWord}>
+        <input name='w_1' value={state.w_1} onChange={handleInputChange} maxLength ="1"></input>
+        <input name="w_2" value={state.w_2} onChange={handleInputChange} maxLength ="1"></input>
+        <button type='submit'>send</button>
+      </form>
+  
+      <div>my name is {state.w_1}</div>
+      <div>my name is {state.w_2}</div>
     </div>
   );
 }
 
-export default App;
+export default App
