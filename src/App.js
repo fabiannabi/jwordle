e0594e83685d4081047e7ba6a1eeb9c295bee34c
fabian <fabiannabi@yahoo.com.mx>
2022-03-05 19:37:54 -0600

    Added word and letter check
    -Changes the color of the input if the letter is correct
    -Checks if the whole string is corect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import React, {useState} from 'react';
+import { isCompositeComponentWithType } from 'react-dom/cjs/react-dom-test-utils.development';
 
 function App() {
 
@@ -9,8 +10,6 @@ function App() {
   const randomWord = "longword".split('')
   
   const todaysWordState={}
-
-
 
   randomWord.forEach((letter, index)=>{
     if(!todaysWordState[letter]){
@@ -21,21 +20,72 @@ function App() {
     }
   })
 
- const inputlist = Object.keys(todaysWordState)
+  const inputlist = Object.keys(todaysWordState)
 
   const [state, setState] = useState(todaysWordState); 
+  const [answer, setAnswer] = useState([]); 
 
 
- function checkWord (event) {
-  event.preventDefault()
-  console.log(state)
- }
+  function checkWord (event) {
+    event.preventDefault()
+    let attemptLetter = Object.values(state)
+    let attemptWord = attemptLetter.join('')
+    
+    wordleCheck (attemptWord)
+    letterCheck (attemptLetter)
+    console.log(answer)
+  }
 
- function handleInputChange(event){
-  event.preventDefault()
-  const value =  event.target.value
-  setState( {...state,[event.target.name]:value  } );
- }
+  function wordleCheck (attemptLetter){
+    if (attemptLetter === randomWord.join('')){
+      console.log('corect')
+    }
+    else{
+      console.log('incorrect')
+    }
+  }
+
+  function letterCheck (attemptWord){
+    let coloIndex = []
+    attemptWord.forEach((letter, i)=>{
+        console.log(randomWord[i])
+      if( randomWord[i] === letter){
+        coloIndex.push(i)
+      }
+      else{
+        return
+      }
+    })
+    colorChange(coloIndex)
+  }
+
+
+function colorChange(coloIndex){
+  let inputs =  document.getElementsByTagName('input');
+  let a= Object.values(inputs)
+  console.log(inputs)
+  coloIndex.forEach((cell)=>{
+    inputs[cell].style.background = 'green'
+  })
+
+  for (var i = 0; i < inputs.length; i++){
+   
+   if(inputs[i].style.background === 'green'){
+   }
+   else{
+    inputs[i].style.background = 'red'
+   }
+  }
+
+}
+
+
+
+  function handleInputChange(event){
+    event.preventDefault()
+    const value =  event.target.value.toLowerCase()
+    setState( {...state,[event.target.name]:value  } );
+  }
 
   return (
     <div className="App">
